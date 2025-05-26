d911afa9a36f711c83628c9ce195774e1bc8c9ca
Adrielly0124 <drika.dantas24@gmail.com>
2025-05-26 16:48:05 -0300

    26/05/2025

--- PHP/curso3_poo/screen-match/index.php
+++ PHP/curso3_poo/screen-match/index.php
@@ -5,10 +5,17 @@ require __DIR__ . "/src/Modelo/Filme.php";
 echo "Bem-vindo(a) ao ScreenMatch\n";
 
 $filme = new Filme();
-$filme-> nome = 'Thor - Ragnarok';
+$filme->defineAnoLancamento(2021);
+/*$filme-> nome = 'Thor - Ragnarok';
 $filme->anolancamento = 2021;
-$filme->genero = 'super-heroi';
-$filme->media = 8.8;
+$filme->genero = 'super-heroi';*/
+$filme->avalia(10);
+$filme->avalia(10);
+$filme->avalia(5);
+$filme->avalia(5);
 
 var_dump($filme);
 
+echo $filme->media() . "\n";
+
+echo $filme->anoLancamento();
--- PHP/curso3_poo/screen-match/src/Modelo/Filme.php
+++ PHP/curso3_poo/screen-match/src/Modelo/Filme.php
@@ -1,13 +1,29 @@
 <?php
 
 class Filme {
-    public string $nome;
-    public int $anoLancamento;
-    public string $genero;
-    public float $media;
-    public array $notas = [];
+    private string $nome = 'Nome Padrão';
+    private int $anoLancamento = 2024;
+    private string $genero = 'Ação';
+    private float $media;
+    private array $notas = [];
 
-    function avalia(float $nota){
-
+    public function avalia(float $nota):void{
+        $this->notas[] = $nota;
     }
+
+    public function media(): float{
+        $somaNotas = array_sum($this->notas);
+        $quantidadeNotas = count($this->notas);
+        return $somaNotas / $quantidadeNotas;
+    }
+
+    public function anoLancamento(): int {
+        return $this->anoLancamento; //método público para acessar uma propriedade privada 
+    }
+
+    public function defineAnoLancamento(int $anoLancamento) {
+        $this->anoLancamento = $anoLancamento;
+    }
+
 }
+
